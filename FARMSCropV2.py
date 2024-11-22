a638af17db02554d1c8bc6057148150baa86cdc8
Clybius <55800048+Clybius@users.noreply.github.com>
2024-11-22 14:51:34 -0600

    Feat: Update FARMSCropV2 to V2.1
    Replace momentum_amp with a scaling exponent, add clamping to the natural gradients for stability, adjust eps so we're not adding eps every step.

--- FARMSCropV2.py
+++ FARMSCropV2.py
@@ -12,35 +12,35 @@ class FARMSCropV2(Optimizer):
             Learning rate parameter (default 0.0001).
         betas (float, float):
             coefficients used for computing running averages of
-            gradient difference FIM and approx. natural grad FIM (default: 0.999, 0.9999).
+            gradient difference FIM and approx. natural grad FIM (default: 0.9, 0.9999).
         eps (float):
-            Term added to the denominator outside of the root operation to
-            improve numerical stability. (default: 1e-8).
+            Term the denominator is minimally clamped to, to
+            improve numerical stability. (default: 1e-6).
         weight_decay (float):
-            Weight decay, i.e. a L2 penalty (default: 1e-6).
+            Weight decay, i.e. a L2 penalty (default: 0.0).
         centralization (float):
-            Center model grad (default: 0.5).
+            Center model grad (default: 0.0).
         diff_mult (float):
             Multiplier for difference amplification (default: 0.25).
         momentum_beta (float):
             Beta value for slow momentum / EMA (default: 0.9999).
-        momentum_amp (float):
-            Amplification multiplier for slow momentum / EMA (default: 5.0).
+        momentum_lambda (float):
+            Amplification exponent for slow momentum / EMA (default: 0.25).
         clip (float):
-            Value to clip at (default: 1.0)
+            Value to clip the grad's RMS at (default: 1.0)
     """
 
     def __init__(
         self,
         params,
         lr=1e-4,
-        betas=(0.999, 0.9999),
-        eps=1e-8,
-        weight_decay=1e-6,
-        centralization=0.5,
+        betas=(0.9, 0.9999),
+        eps=1e-6,
+        weight_decay=0.0,
+        centralization=0.0,
         diff_mult=0.25,
         momentum_beta=0.9999,
-        momentum_amp=5.0,
+        momentum_lambda=0.25,
         clip=1.0,
     ):
         defaults = dict(
@@ -51,7 +51,7 @@ class FARMSCropV2(Optimizer):
             centralization=centralization,
             diff_mult=diff_mult,
             momentum_beta=momentum_beta,
-            momentum_amp=momentum_amp,
+            momentum_lambda=momentum_lambda,
             clip=clip,
         )
         super(FARMSCropV2, self).__init__(params, defaults)
@@ -90,9 +90,11 @@ class FARMSCropV2(Optimizer):
                 weight_decay = group["weight_decay"]
                 centralization = group["centralization"]
                 momentum_beta = group["momentum_beta"]
-                momentum_amp = group["momentum_amp"]
+                momentum_lambda = group["momentum_lambda"]
                 clip = group["clip"]
                 state["step"] += 1
+
+                clip_lambda = state["step"]**0.25
 
                 fim_slow_beta = ((beta2**state["step"] - beta2) / (beta2**state["step"] - 1.0)) ** (1/2)
 
@@ -105,7 +107,7 @@ class FARMSCropV2(Optimizer):
                     grad_diff_fim = state["grad_diff_fim"]
 
                     # Get natural gradient (squared ema, obtained sqrt of ema)
-                    diff_fim_base = grad_diff_fim.sqrt().add_(group["eps"])
+                    diff_fim_base = torch.clamp(grad_diff_fim.sqrt(), group["eps"])
                     grad_diff_fim.mul_(beta1).addcmul_(grad_diff, grad_diff, value=1 - beta1)
                 else:
                     diff_fim_base = 1.0
@@ -114,16 +116,18 @@ class FARMSCropV2(Optimizer):
                 rms = approx_grad_nat.pow(2).mean().sqrt_()
                 divisor = max(clip, rms) / clip
                 approx_grad_nat.div_(divisor)
+                approx_grad_nat.clamp_(-clip_lambda, clip_lambda)
 
-                fim_base = fim.sqrt().add_(group["eps"])
+                fim_base = torch.clamp(fim.sqrt(), group["eps"])
 
                 grad_nat = grad.div(fim_base).mul_(diff_fim_base)
                 rms = grad_nat.pow(2).mean().sqrt_()
                 divisor = max(clip, rms) / clip
                 grad_nat.div_(divisor)
+                grad_nat.clamp_(-clip_lambda, clip_lambda)
 
                 # Compass-style amplification
-                full_step = grad_nat.add(momentum, alpha=momentum_amp)
+                full_step = grad_nat.add(momentum, alpha=state["step"]**momentum_lambda)
 
                 # center the gradient vector
                 if centralization != 0 and full_step.dim() > 1:
